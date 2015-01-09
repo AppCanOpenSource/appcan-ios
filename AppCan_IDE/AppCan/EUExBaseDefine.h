dbb5461121a5da85fc2d8c27f52f1fd877728c41
AppCanOpenSource <guangqiao.li@3g2win.com>
2015-01-09 14:57:32 +0800

    增加GNU LGPL v3.0
    增加GNU LGPL v3.0

--- AppCan_IDE/AppCan/EUExBaseDefine.h
+++ AppCan_IDE/AppCan/EUExBaseDefine.h
@@ -1,10 +1,20 @@
-//
-//  EUExBaseDefine.h
-//  WBPalmLib
-//
-//  Created by 邹 达 on 12-4-17.
-//  Copyright 2012 zywx. All rights reserved.
-//
+/*
+ *  Copyright (C) 2014 The AppCan Open Source Project.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ 
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #import <Foundation/Foundation.h>
 
