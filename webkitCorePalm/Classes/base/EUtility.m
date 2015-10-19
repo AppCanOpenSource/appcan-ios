d11aa50726c59dcf6c1cee2316d7531117e0fe52
connyliu <21165491@qq.com>
2015-10-19 12:09:54 +0800

    EUtility  扩展回调方法

--- webkitCorePalm/Classes/base/EUtility.m
+++ webkitCorePalm/Classes/base/EUtility.m
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
+#import "JSON.h"
 #import "EUtility.h"
 #import "BUtility.h"
 #import "EBrowserView.h"
@@ -31,6 +31,10 @@
 #import "ACEDrawerViewController.h"
 #import "ACEPluginViewContainer.h"
 
+
+NSString * const cUexPluginCallbackInRootWindow = @"uexPluginCallbackInRootWindow";
+NSString * const cUexPluginCallbackInFrontWindow = @"uexPluginCallbackInFrontWindow";
+
 void PluginLog (NSString *format, ...) {
     #ifdef Plugin_OUTPUT_LOG_CONSOLE
 	va_list args;
@@ -43,6 +47,47 @@ void PluginLog (NSString *format, ...) {
 }
 
 @implementation EUtility
+
+
+
+//2015-09-23 by lkl
++ (void)uexPlugin:(NSString *)pluginName callbackByName:(NSString *)functionName withObject:(id)obj andType:(uexPluginCallbackType)type inTarget:(id)target{
+    
+    NSString * result=[obj JSONFragment];
+    NSString * callbackString;
+    switch (type) {
+        case uexPluginCallbackWithJsonString: {
+            callbackString = [NSString stringWithFormat:@"if(%@.%@ != null){%@.%@('%@');}",pluginName,functionName,pluginName,functionName,result];
+            break;
+        }
+        case uexPluginCallbackWithJsonObject: {
+            callbackString = [NSString stringWithFormat:@"if(%@.%@ != null){%@.%@(%@);}",pluginName,functionName,pluginName,functionName,result];
+            break;
+        }
+
+    }
+    
+    if(target == cUexPluginCallbackInRootWindow){
+        [self evaluatingJavaScriptInRootWnd:callbackString];
+    }else if(target == cUexPluginCallbackInFrontWindow){
+        [self evaluatingJavaScriptInFrontWnd:callbackString];
+    }else if(target && [target isKindOfClass:[EBrowserView class]]){
+        EBrowserView * inBrwView=(EBrowserView *)target;
+        [self brwView:inBrwView evaluateScript:callbackString];
+    }
+    
+}
+
+
++(NSBundle *)bundleForPlugin:(NSString *)pluginName{
+    NSString * bundleName = [NSString stringWithFormat:@"%@.bundle",pluginName];
+    NSString * bundlePath = [[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:bundleName];
+    return [NSBundle bundleWithPath:bundlePath];
+}
+
+
+
+
 +(NSString*)makeUrl:(NSString*)inBaseStr url:(NSString*)inUrl {
 	return [BUtility makeUrl:inBaseStr url:inUrl];
 }
@@ -363,11 +408,8 @@ return [BUtility isConnected];
 //    app.drawerController.isGestureRecognizer = isEnable;
 }
 
-+(NSBundle *)bundleForPlugin:(NSString *)pluginName{
-    NSString * bundleName = [NSString stringWithFormat:@"%@.bundle",pluginName];
-    NSString * bundlePath = [[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:bundleName];
-    return [NSBundle bundleWithPath:bundlePath];
-}
+
+
 
 +(NSString *)uexPlugin:(NSString *)pluginName localizedString:(NSString *)key,...{
     NSBundle *pluginBundle = [self bundleForPlugin:pluginName];
@@ -378,10 +420,12 @@ return [BUtility isConnected];
     va_list argList;
     va_start(argList,key);
     id arg=va_arg(argList,id);
-    if(arg && [arg isKindOfClass:[NSString class]]){
+    //if(arg && [arg isKindOfClass:[NSString class]]){
+    if(arg){
         defaultValue=arg;
     }
     va_end(argList);
     return [pluginBundle localizedStringForKey:key value:defaultValue table:nil];
 }
+
 @end
