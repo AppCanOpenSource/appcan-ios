eac4360241ec2f8e0783c0307ec50f4f1a671893
connyliu <21165491@qq.com>
2016-01-21 12:47:13 +0800

    修复EUExBase里的三个JS回调方法在非主线程运行时会导致alert卡死的问题

--- webkitCorePalm/Classes/engine/universalex/EUExBase.m
+++ webkitCorePalm/Classes/engine/universalex/EUExBase.m
@@ -26,86 +26,93 @@
 #import "WWidget.h"
 #import "EBrowserWindow.h"
 #import "WidgetOneDelegate.h"
+#import "EUtility.h"
 @implementation EUExBase
 @synthesize meBrwView;
 
-- (id)initWithBrwView:(EBrowserView *) eInBrwView{	
-	if (self=[super init]) {
-		meBrwView = eInBrwView;
-	}
-	return self;
+- (id)initWithBrwView:(EBrowserView *) eInBrwView{
+    if (self=[super init]) {
+        meBrwView = eInBrwView;
+    }
+    return self;
 }
 
 - (void)stopNetService {
 }
 
 - (void)dealloc{
-	//ACENSLog(@"EUExBase retain count is %d",[self retainCount]);
-	//ACENSLog(@"EUExBase dealloc is %x", self);
+    //ACENSLog(@"EUExBase retain count is %d",[self retainCount]);
+    //ACENSLog(@"EUExBase dealloc is %x", self);
     if (meBrwView) {
         meBrwView = nil;
     }
-	[super dealloc];
+    [super dealloc];
 }
 
 - (void)jsSuccessWithName:(NSString *)inCallbackName opId:(int)inOpId dataType:(int)inDataType strData:(NSString*)inData {
-	//inData = [inData stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-	NSString *jsSuccessStr = [NSString stringWithFormat:@"if(%@!=null){%@(%d,%d,\'%@\');}",inCallbackName,inCallbackName,inOpId,inDataType,inData];
-	ACENSLog(@"jsSuccessStr=%@",jsSuccessStr);
-	[meBrwView stringByEvaluatingJavaScriptFromString:jsSuccessStr];
+    //inData = [inData stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *jsSuccessStr = [NSString stringWithFormat:@"if(%@!=null){%@(%d,%d,\'%@\');}",inCallbackName,inCallbackName,inOpId,inDataType,inData];
+    ACENSLog(@"jsSuccessStr=%@",jsSuccessStr);
+    [EUtility brwView:self.meBrwView evaluateScript:jsSuccessStr];
+    
 }
 
 - (void)jsSuccessWithName:(NSString *)inCallbackName opId:(int)inOpId dataType:(int)inDataType intData:(int)inData {
-	NSString *jsSuccessStr = [NSString stringWithFormat:@"if(%@!=null){%@(%d,%d,%d);}",inCallbackName,inCallbackName,inOpId,inDataType,inData];
-	ACENSLog(@"jsSuccessStr=%@",jsSuccessStr);
-	[meBrwView stringByEvaluatingJavaScriptFromString:jsSuccessStr];
+    NSString *jsSuccessStr = [NSString stringWithFormat:@"if(%@!=null){%@(%d,%d,%d);}",inCallbackName,inCallbackName,inOpId,inDataType,inData];
+    ACENSLog(@"jsSuccessStr=%@",jsSuccessStr);
+    [EUtility brwView:self.meBrwView evaluateScript:jsSuccessStr];
+    
 }
 
 - (void)jsFailedWithOpId:(int)inOpId errorCode:(int)inErrorCode errorDes:(NSString*)inErrorDes {
-	inErrorDes =[inErrorDes stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-	NSString *jsFailedStr = [NSString stringWithFormat:@"if(uexWidgetOne.cbError!=null){uexWidgetOne.cbError(%d,%d,\'%@\');}",inOpId,inErrorCode,inErrorDes];
-	ACENSLog(@"jsFailedStr=%@",jsFailedStr);
-	[meBrwView stringByEvaluatingJavaScriptFromString:jsFailedStr];
+    inErrorDes =[inErrorDes stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *jsFailedStr = [NSString stringWithFormat:@"if(uexWidgetOne.cbError!=null){uexWidgetOne.cbError(%d,%d,\'%@\');}",inOpId,inErrorCode,inErrorDes];
+    ACENSLog(@"jsFailedStr=%@",jsFailedStr);
+    [EUtility brwView:self.meBrwView evaluateScript:jsFailedStr];
+    
 }
 
 - (void)clean{
 }
 
 -(NSString*)absPath:(NSString*)inPath{
-    
+    ACENSLog(@"inpath start=%@",inPath);
     inPath = [inPath stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
+    if ([inPath hasPrefix:@"file://"]) {
+        inPath = [inPath substringFromIndex:[@"file://" length]];
+        return inPath;
+    }
     if ([inPath hasPrefix:@"/var/mobile"]||[inPath hasPrefix:@"assets-library"]||[inPath hasPrefix:@"/private/var/mobile"]||[inPath hasPrefix:@"/Users"]||[inPath hasPrefix:@"file://"]) {
-		return inPath;
-	}
-	NSURL *curURL = [self.meBrwView curUrl];
-	inPath = [BUtility makeUrl:[curURL absoluteString] url:inPath];
-	//box://
-	if ([inPath hasPrefix:F_BOX_PATH]) {
-		NSString * str = [BUtility getDocumentsPath:@"box"];
-		if (![[NSFileManager defaultManager] fileExistsAtPath:str]) {
-			[[NSFileManager defaultManager] createDirectoryAtPath:str withIntermediateDirectories:YES attributes:nil error:nil];
-		}
-		NSString *resultStr = [NSString stringWithFormat:@"%@/%@",str,[inPath substringFromIndex:6]];
-		//NSLog(@"str=%@",resultStr);
-		return resultStr;
-	}
-	if ([inPath hasPrefix:F_WGTS_PATH]||[inPath hasPrefix:F_APP_PATH]||[inPath hasPrefix:F_RES_PATH]) {
-//		EBrowserWindowContainer *eBrwWndContainer = (EBrowserWindowContainer*)meBrwView.meBrwWnd.superview;
+        return inPath;
+    }
+    NSURL *curURL = [self.meBrwView curUrl];
+    inPath = [BUtility makeUrl:[curURL absoluteString] url:inPath];
+    //box://
+    if ([inPath hasPrefix:F_BOX_PATH]) {
+        NSString * str = [BUtility getDocumentsPath:@"box"];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:str]) {
+            [[NSFileManager defaultManager] createDirectoryAtPath:str withIntermediateDirectories:YES attributes:nil error:nil];
+        }
+        NSString *resultStr = [NSString stringWithFormat:@"%@/%@",str,[inPath substringFromIndex:6]];
+        //NSLog(@"str=%@",resultStr);
+        return resultStr;
+    }
+    if ([inPath hasPrefix:F_WGTS_PATH]||[inPath hasPrefix:F_APP_PATH]||[inPath hasPrefix:F_RES_PATH]) {
+        //		EBrowserWindowContainer *eBrwWndContainer = (EBrowserWindowContainer*)meBrwView.meBrwWnd.superview;
         
         EBrowserWindowContainer *eBrwWndContainer = [EBrowserWindowContainer getBrowserWindowContaier:meBrwView];
-		NSString *absPath =nil;
-		absPath=[self.meBrwView.meBrwCtrler.mwWgtMgr curWidgetPath:eBrwWndContainer.mwWgt];
-		ACENSLog(@"abspath=%@",absPath);
-		NSString *relativePath=nil;
-		if ([inPath hasPrefix:F_APP_PATH]) {
-			relativePath =[inPath substringFromIndex:6];
-		}
-		if ([inPath hasPrefix:F_RES_PATH]) {
-			relativePath =[inPath substringFromIndex:6];
-			if (eBrwWndContainer.mwWgt.wgtType==F_WWIDGET_MAINWIDGET) {
+        NSString *absPath =nil;
+        absPath=[self.meBrwView.meBrwCtrler.mwWgtMgr curWidgetPath:eBrwWndContainer.mwWgt];
+        ACENSLog(@"abspath=%@",absPath);
+        NSString *relativePath=nil;
+        if ([inPath hasPrefix:F_APP_PATH]) {
+            relativePath =[inPath substringFromIndex:6];
+        }
+        if ([inPath hasPrefix:F_RES_PATH]) {
+            relativePath =[inPath substringFromIndex:6];
+            if (eBrwWndContainer.mwWgt.wgtType==F_WWIDGET_MAINWIDGET) {
                 BOOL isCopyFinish = [[[NSUserDefaults standardUserDefaults]objectForKey:F_UD_WgtCopyFinish] boolValue];
-				if (theApp.useUpdateWgtHtmlControl && isCopyFinish) {
+                if (theApp.useUpdateWgtHtmlControl && isCopyFinish) {
                     if ([BUtility getSDKVersion]<5.0) {
                         absPath =[BUtility getCachePath:@"widget/wgtRes"];
                     }else {
@@ -114,22 +121,22 @@
                 }else {
                     absPath =[BUtility getResPath:@"widget/wgtRes"];
                 }
-			}else {
-				absPath = [NSString stringWithFormat:@"%@/wgtRes",absPath];
-			}
-			ACENSLog(@"absPath middle=%@",absPath);
-			
-		}
-		if ([inPath hasPrefix:F_WGTS_PATH]) {
-			absPath = [BUtility getDocumentsPath:@"widgets"];
-			relativePath =[inPath substringFromIndex:7]; 
-		}
-		inPath = [NSString stringWithFormat:@"%@/%@",absPath,relativePath];
-		ACENSLog(@"inPath end=%@",inPath);
-	}
-	if ([inPath hasPrefix:@"file://"]) {
-		inPath = [inPath substringFromIndex:[@"file://" length]];
-	}
-	return inPath;
+            }else {
+                absPath = [NSString stringWithFormat:@"%@/wgtRes",absPath];
+            }
+            ACENSLog(@"absPath middle=%@",absPath);
+            
+        }
+        if ([inPath hasPrefix:F_WGTS_PATH]) {
+            absPath = [BUtility getDocumentsPath:@"widgets"];
+            relativePath =[inPath substringFromIndex:7]; 
+        }
+        inPath = [NSString stringWithFormat:@"%@/%@",absPath,relativePath];
+        ACENSLog(@"inPath end=%@",inPath);
+    }
+    if ([inPath hasPrefix:@"file://"]) {
+        inPath = [inPath substringFromIndex:[@"file://" length]];
+    }
+    return inPath;
 }
 @end
