2fb2086466e59b3f96d2d3daa15148dd28167670
connyliu <21165491@qq.com>
2016-09-27 17:04:12 +0800

    容器 移除滚动条

--- webkitCorePalm/Classes/engine/ACEPluginViewContainer.m
+++ webkitCorePalm/Classes/engine/ACEPluginViewContainer.m
@@ -12,39 +12,29 @@
 @implementation ACEPluginViewContainer
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    
     if (self = [super initWithFrame:frame]) {
-        
         _maxIndex = -1;
-        
         self.pagingEnabled = YES;
-        
         self.bounces = NO;
-        
         self.delegate = self;
-        
         _lastIndex = -1;
-        
+        self.showsHorizontalScrollIndicator = NO;
+        self.showsVerticalScrollIndicator = NO;
     }
-    
     return self;
     
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    
     CGFloat pageWidth = scrollView.bounds.size.width ;
     float fractionalPage = scrollView.contentOffset.x / pageWidth ;
     NSInteger index = lround(fractionalPage) ;
-    
     [self performSelectorOnMainThread:@selector(onPluginContainerPageChange:) withObject:[NSString stringWithFormat:@"%ld",(long)index] waitUntilDone:NO];
     
 }
 
 - (void)onPluginContainerPageChange:(id)userInfo {
-    
     NSInteger index = [(NSString *)userInfo integerValue];
-    
     if (_lastIndex != index) {
         _lastIndex = index;
         [_uexObj jsSuccessWithName:@"uexWindow.onPluginContainerPageChange" opId:self.containerIdentifier dataType:1 intData:index];
@@ -53,16 +43,10 @@
 }
 
 - (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
-    
     CGFloat pageWidth = scrollView.bounds.size.width ;
     float fractionalPage = scrollView.contentOffset.x / pageWidth ;
-    NSInteger index = lround(fractionalPage) ;
-
+    NSInteger index = lround(fractionalPage);
     [self performSelectorOnMainThread:@selector(onPluginContainerPageChange:) withObject:[NSString stringWithFormat:@"%ld",(long)index] waitUntilDone:NO];
-    
-    
-    
-    
 }
 
 /*
