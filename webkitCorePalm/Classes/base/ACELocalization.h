140bf425818431eec2d34e552b8abdb6c1966933
xrg <xurigone@163.com>
2016-11-24 14:39:06 +0800

    国际化相关修改
    修复引擎中部分提示信息仅能跟随系统切换语言，不能被uexAppLanguage控制的问题

--- webkitCorePalm/Classes/base/ACELocalization.h
+++ webkitCorePalm/Classes/base/ACELocalization.h
@@ -11,18 +11,16 @@
 #define ACELocalized(...)   \
 [ACELocalization localizedString:__VA_ARGS__,nil]
 
-
-
-
-
 @interface ACELocalization : NSObject
-
-
 
 +(NSString*)localizedString:(NSString *)key, ...;
 
++ (NSString *)localizedStringForKey:(NSString *)key defaultValue:(NSString *)value;
 
++ (BOOL)isUseSystemLanguage;
 
++ (NSString *)getAppCanUserLanguage;
+
++ (NSBundle *)languageBundle;
 
 @end
-
