b77c8f464a0179542c242fb03b187c3b9dadd9b7
connyliu <21165491@qq.com>
2017-02-09 13:18:55 +0800

    支持一键生成引擎zip包

---
+++ AppCanEnginePackager/AppCanEnginePackager/EngineInfo.swift
@@ -0,0 +1,162 @@
+//
+//  EngineInfo.swift
+//  AppCanEnginePackager
+//
+//  Created by CeriNo on 2017/2/9.
+//  Copyright © 2017年 AppCan. All rights reserved.
+//
+
+import Foundation
+
+struct Version {
+    let major: Int
+    let minor: Int
+    let build: Int
+    static func from(string: String) -> Version {
+        var helper = [0,0,0]
+        let nums = string.components(separatedBy: ".")
+        for i in 0..<3 {
+            guard i < nums.count else {
+                break
+            }
+            helper[i] = Int(nums[i]) ?? 0
+        }
+        return Version(major: helper[0], minor: helper[1], build: helper[2])
+    }
+}
+
+
+
+extension Version: Comparable{
+    private var versionNumber: Int{
+        get {
+            return build + minor * 10000 + major * 1000000
+        }
+    }
+    public static func ==(lhs: Version, rhs: Version) -> Bool{
+        return lhs.versionNumber == rhs.versionNumber
+    }
+    public static func <(lhs: Version, rhs: Version) -> Bool {
+        return lhs.versionNumber < rhs.versionNumber
+    }
+}
+
+
+struct EngineInfo {
+    let suffix: String
+    let version: Version
+    let description: String
+    let buildNumber: Int
+    let date: String
+    let xcodeVersion = "8.1"
+
+}
+
+extension EngineInfo{
+
+    
+    static func parseChangelog() -> (version: Version,summary: String){
+        let changelogURL = rootURL.appendingPathComponent("AppCanEngine").appendingPathComponent("Changelog.plist")
+        let fm = FileManager.default
+        print(changelogURL.path)
+        guard fm.fileExists(atPath: changelogURL.path) else{
+            fatalError("Changelog.plist not exist")
+        }
+        guard
+            let plistData = try? Data(contentsOf: changelogURL),
+            let plist = (try? PropertyListSerialization.propertyList(from: plistData, format: nil))
+            else{
+                fatalError("Changelog.plist Invalid!")
+        }
+
+        guard let dict = plist as? [String: [String]] else{
+            fatalError("Changelog.plist format error!")
+        }
+        
+        let lastVersion = Array(dict.keys).sorted {
+            Version.from(string: $0) < Version.from(string: $1)
+        }.last!
+        let version = Version.from(string: lastVersion)
+        let summary = dict[lastVersion]!.joined(separator: ";")
+        return (version,summary)
+    }
+    
+    init(versionString: String?,summary: String?,suffix: String?) {
+        if
+            let versionString = versionString,
+            let summary = summary,
+            versionString.characters.count > 0{
+            version = Version.from(string: versionString)
+            description = summary
+        }else{
+            (version,description) = EngineInfo.parseChangelog()
+        }
+        if let suffix = suffix,suffix.characters.count > 0{
+            self.suffix = "_\(suffix)"
+        }else{
+            self.suffix = ""
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyMMdd"
+        let date = formatter.string(from: Date())
+        self.date = date
+        
+        let lastBuildDateKey = "kLastBuildDate"
+        let buildNumberKey = "kBuildNumber"
+        let ud = UserDefaults.standard
+        
+        
+        guard let lastBuildDate = ud.string(forKey: lastBuildDateKey),lastBuildDate == date else{
+            ud.set(date, forKey: lastBuildDateKey)
+            ud.set(0, forKey: buildNumberKey)
+            buildNumber = 0
+            return
+        }
+        let number = ud.integer(forKey: buildNumberKey) + 1
+        ud.set(number, forKey: buildNumberKey)
+        buildNumber = number
+    }
+}
+
+
+extension EngineInfo{
+    
+    var build: String{
+        get{
+            return buildNumber < 10 ? "0\(self.buildNumber)" : "\(buildNumber)"
+        }
+    }
+    
+    var package: String{
+        get{
+            return "iOS_Engine_\(version.major).\(version.minor).\(version.build)_\(date)_\(build)_Xcode\(xcodeVersion)\(suffix)"
+        }
+    }
+    private var dssVersion: String{
+        get {
+            return "4.0.0"
+        }
+    }
+    private var versionString: String{
+        get{
+            return "sdksuit_\(version.major).\(version.minor)_\(date)_\(build)_Xcode\(xcodeVersion)\(suffix)"
+        }
+    }
+    
+
+    
+    func XMLInfo() -> String{
+        var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+        xml += "<info>\n"
+        xml += "\t<version>\(versionString)</version>\n"
+        xml += "\t<package>\(package)</package>\n"
+        xml += "\t<description>\(description)</description>\n"
+        xml += "\t<dssVersion>\(dssVersion)</dssVersion>\n"
+        xml += "</info>"
+        return xml
+    }
+    
+}
+
+
---
+++ AppCanEnginePackager/AppCanEnginePackager/main.swift
@@ -0,0 +1,94 @@
+//
+//  main.swift
+//  AppCanEnginePackager
+//
+//  Created by CeriNo on 2017/2/8.
+//  Copyright © 2017年 AppCan. All rights reserved.
+//
+
+import Foundation
+import LzmaSDK_ObjC
+import Swiftline
+import Zip
+
+
+var rootURL: URL
+
+
+let args = Args.parsed.flags
+guard let srcPath = args["src"] else{
+    fatalError("Invalid Args")
+}
+rootURL = URL(fileURLWithPath: srcPath).deletingLastPathComponent()
+guard rootURL.lastPathComponent == "appcan-ios" else{
+    fatalError("Invalid rootURL")
+}
+
+//MARK: - build engine
+let engineBuildCommand: (String) -> Void = { command in
+    let xcodebuild = XcodeBuild(path: .workspace(path: "AppCanEngine.xcworkspace"),
+                                command: command,
+                                configuration: "Release",
+                                scheme: "AppCanEngine",
+                                sdk: "iphoneos",
+                                currentDirectoryPath: rootURL.path)
+    xcodebuild.runSync()
+}
+
+engineBuildCommand("clean")
+engineBuildCommand("build")
+
+
+let info = EngineInfo(versionString: args["version"], summary: args["summary"], suffix: args["suffix"])
+
+
+do{
+    
+    let archiveFolderURL = rootURL.appendingPathComponent("archives")
+    let tmpFolderURL = archiveFolderURL.appendingPathComponent("tmp")
+    let fm = FileManager.default
+    var isFolder: ObjCBool = false
+//MARK: create archive folder
+    if !fm.fileExists(atPath: archiveFolderURL.path, isDirectory: &isFolder) || !isFolder.boolValue{
+        try fm.createDirectory(at: archiveFolderURL, withIntermediateDirectories: true, attributes: nil)
+    }
+//MARK: create tmp folder
+    if fm.fileExists(atPath: tmpFolderURL.path){
+        try fm.removeItem(at: tmpFolderURL)
+    }
+    try fm.createDirectory(at: tmpFolderURL, withIntermediateDirectories: true, attributes: nil)
+    
+//MARK: generate 7z package
+    let _7zURL = tmpFolderURL.appendingPathComponent("package.7z")
+    let writer = LzmaSDKObjCWriter(fileURL: _7zURL)
+    guard writer.addPath(rootURL.appendingPathComponent("AppCanPlugin").path, forPath: "AppCanPlugin") else{
+        fatalError("AppCanPlugin not exist")
+    }
+    writer.method = LzmaSDKObjCMethodLZMA2 // or LzmaSDKObjCMethodLZMA
+    writer.compressionLevel = 9
+    try writer.open()
+    writer.write()
+    let packageURL = tmpFolderURL.appendingPathComponent(info.package)
+    try fm.moveItem(atPath: _7zURL.path, toPath: packageURL.path)
+    
+//MARK: generate engineInfo.xml
+    let xml = info.XMLInfo()
+    let xmlURL = tmpFolderURL.appendingPathComponent("iosEngine.xml")
+    try xml.write(to: xmlURL, atomically: true, encoding: .utf8)
+    
+//MARK: export engine archive
+    let archiveURL = archiveFolderURL.appendingPathComponent("\(info.package).zip")
+    if fm.fileExists(atPath: archiveURL.path){
+        try fm.removeItem(at: archiveURL)
+    }
+    try Zip.zipFiles(paths: [packageURL,xmlURL], zipFilePath: archiveURL, password: nil, progress: nil)
+    
+//MARK: clean up
+    try fm.removeItem(at: tmpFolderURL)
+
+}catch let e as NSError{
+    fatalError("error: \(e.localizedDescription)")
+}
+
+
+
